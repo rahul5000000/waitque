ffaaf342f54e0b2e0283a65c39de4718b2a60d48
rahul5000000 <rahul@therrsgroup.com>
2025-08-18 23:55:32 -0400

    1: Integrate app with Postgres

--- src/main/java/com/rrsgroup/waitque/web/UserController.java
+++ src/main/java/com/rrsgroup/waitque/web/UserController.java
@@ -1,10 +1,8 @@
 package com.rrsgroup.waitque.web;
 
-import com.rrsgroup.waitque.domain.UserRole;
 import com.rrsgroup.waitque.dto.UserDto;
 import com.rrsgroup.waitque.exception.RoleNotFoundException;
 import com.rrsgroup.waitque.service.UserService;
-import com.rrsgroup.waitque.util.JwtWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,8 +10,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
 
 @RestController
 public class UserController {
