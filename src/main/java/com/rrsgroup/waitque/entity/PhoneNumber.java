f9a371d2b575b7551b785c3ec3ed1ec81065530b
rahul5000000 <rahul@therrsgroup.com>
2025-08-19 21:43:19 -0400

    3: Add phone number entity

---
+++ src/main/java/com/rrsgroup/waitque/entity/PhoneNumber.java
@@ -0,0 +1,15 @@
+package com.rrsgroup.waitque.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class PhoneNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer countryCode;
+    private Long phoneNumber;
+}
