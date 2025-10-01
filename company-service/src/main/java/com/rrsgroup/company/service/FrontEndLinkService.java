95d7a62c66975fa8986b1b35ff740a65b24c0ecf
rahul5000000 <rahul@therrsgroup.com>
2025-10-01 11:28:29 -0400

    Customize QR code url based on environment

--- company-service/src/main/java/com/rrsgroup/company/service/FrontEndLinkService.java
+++ company-service/src/main/java/com/rrsgroup/company/service/FrontEndLinkService.java
@@ -1,12 +1,21 @@
 package com.rrsgroup.company.service;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Log4j2
 public class FrontEndLinkService {
+    @Value("${self.base-url}")
+    private String selfBaseUrl;
+
     public String getCustomerLandingPageLink(Long companyId, UUID customerCode) {
-        return "https://waitque.com/web/landing?company="+companyId+"&customerCode="+customerCode;
+        String customerLandingPageLink = selfBaseUrl+"/web/landing?company="+companyId+"&customerCode="+customerCode;
+        log.debug("customerLandingPageLink: {}", customerLandingPageLink);
+
+        return customerLandingPageLink;
     }
 }
